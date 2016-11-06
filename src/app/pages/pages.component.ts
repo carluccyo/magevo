c72be3aed47fca39a250807af803e98b83a9af2d
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-06 22:48:36 +0100

    set home module set page module

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,17 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+@Component({
+  selector: 'pages',
+  encapsulation: ViewEncapsulation.None,
+  styles: [],
+  template: `
+    <div>pages</div>
+    `
+})
+export class Pages {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }      from '@angular/core';
+import { CommonModule }  from '@angular/common';
+
+import { routing }       from './pages.routing';
+
+
+import { Pages } from './pages.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    routing],
+  declarations: [Pages]
+})
+export class PagesModule {
+}
