093e23d553a2a71c9073d6b4790549b52139c926
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-08 13:14:20 +0100

    format

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,14 @@ import { Logger } from 'angular2-logger/core';
     template: `
     <nav class="navbar navbar-light navbar-dark bg-primary bg-faded">
 
-      <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+      <button class="navbar-toggler hidden-lg-up"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+      </button>
 
       <div class="collapse navbar-toggleable-md" id="navbarResponsive">
 
@@ -26,15 +33,6 @@ import { Logger } from 'angular2-logger/core';
             <a class="nav-link"  [routerLink]="['dashboard']">Dashboard</a>
           </li>
 
-          <!-- <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="responsiveNavbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="responsiveNavbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li> -->
-
         </ul>
 
       </div>
@@ -48,7 +46,7 @@ export class NavbarComponent implements OnInit {
     constructor(private logger: Logger) { }
 
     ngOnInit() {
-      
+
     }
 
 }
