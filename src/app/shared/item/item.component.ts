6afda29301bb2f2cac96d397058b87ef341b57f7
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-12 22:33:57 +0100

    mv bootstrap from 4-alpha to  3.3.7

--- src/app/shared/item/item.component.ts
+++ src/app/shared/item/item.component.ts
@@ -5,36 +5,83 @@ import { Item } from './item.model'
     selector: 'item-detail',
     template: `
     <div class="row item-box">
+
       <div class="col-sm-12 col-xs-12">
 
-        <div class=" item-media">
-          <div class=""></div>
-          <img src={{item.mediaUrl}} itemprop="image" alt="ciao" class="rounded-top" />
+        <div class="col-sm-6 col-xs-6">
+
+          <div class="clear"></div>
+
+          <img src={{item.mediaUrl}} itemprop="image" alt="ciao" class="rounded-top col-sm-12 col-xs-12" />
+
         </div>
 
-        <div>.col-sm-10</div>
+        <div class="col-sm-6 col-xs-6">
+
+          <div class="time">16:13 - 12 novembre 2016</div>
+
+          <div class="red">ciaone</div>
+
+      </div>
+
       </div>
 
     </div>
     `,
     styles: [`
-      .item-box{
-        border: 2px solid #ddd;
-        margin: 2px;
-        padding: 5px;
-      }
 
-      .item-media{
-        width: 36%;
-        float: left;
-        position: relative;
-      }
+        .red {
+          border: 1px solid red;
+        }
 
-      .item-media img{
-        width: 100%;
-        display: block;
-        z-index: 2;
-      }
+        .item-desc{
+            margin: 2px;
+        }
+
+
+        .time {
+          text-align: left;
+          font-size: 0.75em !important;
+          color: #006bae;
+          margin:5px;
+        }
+
+        .time {
+            display: block;
+            font-size: 12px;
+            font-family: Roboto, Arial, Helvetica, sans-serif;
+        }
+
+        .time {
+          border: 0 none;
+          font-size: 100%;
+          margin: 0;
+          padding: 0;
+          vertical-align: baseline;
+          font-family: Roboto, Arial, Helvetica, sans-serif;
+        }
+
+        .clear {
+          clear: both;
+        }
+
+        .item-box{
+          border: 2px solid #ddd;
+          margin: 2px;
+          padding: 5px;
+        }
+
+        .item-media{
+          width: 36%;
+          float: left;
+          position: relative;
+        }
+
+        .item-media img{
+          width: 100%;
+          display: block;
+          z-index: 2;
+        }
 
       `]
 })
