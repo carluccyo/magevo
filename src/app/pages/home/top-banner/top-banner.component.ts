dff36c56fab03c7c203d9f30b724f35f95298ec0
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-20 00:45:11 +0100

    top banner component

--- src/app/pages/home/top-banner/top-banner.component.ts
+++ src/app/pages/home/top-banner/top-banner.component.ts
@@ -4,11 +4,15 @@ import { Component, OnInit } from '@angular/core';
     selector: 'top-banner',
     templateUrl: 'top.banner.component.html',
     styles: [` 
-    .topBanner{
-        margin-top: 5px;
-        margin-bottom: 5px;
-        padding: 2px;
-    }
+        .topBanner{
+            margin-top: 5px;
+            margin-bottom: 5px;
+            padding: 2px;
+        }
+
+        .alignImgCenter{
+             margin: 0 auto;
+        }
     `]
 })
 export class TopBannerComponent implements OnInit {
