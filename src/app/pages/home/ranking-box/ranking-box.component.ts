411598708a9b06ffdbd8a4969c43d19a1b4011cb
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-20 02:50:33 +0100

    initial import box-archive component

--- src/app/pages/home/ranking-box/ranking-box.component.ts
+++ src/app/pages/home/ranking-box/ranking-box.component.ts
@@ -1,14 +1,39 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { Logger } from 'angular2-logger/core';
 
+export const RANKING : RankingList = {
+
+    teams: [
+        {
+            id: 1,
+            name: 'Napoli',
+            points: 33, 
+            ranking: 1
+
+        },
+        {
+            id: 2,
+            name: 'Juventus',
+            points: 23, 
+            ranking: 2
+        },
+        {
+            id: 3,
+            name: 'Milan',
+            points: 13, 
+            ranking: 3
+        }
+    ]
+
+}
 
 
 
 export class Team {
     id: number;
     name: string;
-    description: string;
     points: number;
+    ranking: number;
 }
 
 export class RankingList {
@@ -19,6 +44,10 @@ export class RankingList {
 export class RankingBoxService {
 
     constructor(private logger: Logger) { }
+
+    getRankingList() {
+        return Promise.resolve(RANKING);
+    }
 
 }
 
@@ -32,8 +61,11 @@ export class RankingBoxComponent implements OnInit {
 
     constructor(private logger: Logger, private rankingBoxService: RankingBoxService) { }
 
+    rankingList : RankingList;
+
     ngOnInit() {
         this.logger.info('RankingBoxComponent init.........');
+        this.rankingBoxService.getRankingList().then(rankingList => this.rankingList = rankingList);
     }
 
 }
