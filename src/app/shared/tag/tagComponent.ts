5ccf0a3bfba42c0a987aa15df3519d4b1b72a999
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-23 16:42:59 +0100

    initial import tag component

---
+++ src/app/shared/tag/tagComponent.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'tag',
+    template: `
+        <pre>TODO tag template</pre>
+    `
+})
+export class TagComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
