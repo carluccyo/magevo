f9ac106331ac67c4e59e8c4ff62eaddf9dc2e15d
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-25 23:44:56 +0100

    set window type

--- src/app/shared/window/WindowRef.ts
+++ src/app/shared/window/WindowRef.ts
@@ -1,12 +1,14 @@
 import {Injectable} from '@angular/core';
 
-function _window(): any {
+function _window(): Window {
   // return the native window obj
   return window;
 }
 
 @Injectable()
 export class WindowRef {
+
+  length: number;
 
   get nativeWindow(): any {
     return _window();
