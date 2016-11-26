14302495b30a71b4ac18dfdb0544662e834c01cf
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-26 23:50:16 +0100

    initial import toast component

---
+++ src/app/shared/toast/toast.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'toast',
+    template: `
+        <pre>TODO toast template</pre>
+    `
+})
+export class ToastComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+
+}
