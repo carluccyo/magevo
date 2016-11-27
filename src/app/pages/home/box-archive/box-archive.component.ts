e20cd0b7ad6f8c7a53a6e31637434a58f7a3e666
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-27 14:40:47 +0100

    box archive compoent

--- src/app/pages/home/box-archive/box-archive.component.ts
+++ src/app/pages/home/box-archive/box-archive.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Logger } from 'angular2-Logger/core';
+
 
 @Component({
     selector: 'box-archive',
-    templateUrl: 'box-archive.component.html'
+    templateUrl: 'box-archive.component.html',
+    providers: [Logger]
 })
 export class BoxArchiveComponent implements OnInit {
 
-    constructor() { }
+    constructor(private logger: Logger) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.logger.info('box archive init... ');
+    }
 
 }
