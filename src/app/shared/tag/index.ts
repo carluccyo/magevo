3739b3752ae6a83faa1fd4aef6a2eb82e7021cbe
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-27 14:32:54 +0100

    initial import tag list component

--- src/app/shared/tag/index.ts
+++ src/app/shared/tag/index.ts
@@ -1,3 +1,4 @@
 export * from './tag.component';
+export * from './tag.list.component';
 export * from './tag.model';
 export * from './hashtag.model';
---
+++ src/app/shared/tag/tag.list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { TagComponent} from './tag.component';
+
+@Component({
+    selector: 'tag-list',
+    template: `
+        <pre>TODO tag-list template</pre>
+    `
+})
+export class TagListComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
