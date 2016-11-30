69e773ee8fb9a89cdbc4ab189efb681d4b3d33f6
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-29 19:39:27 -0800

    initial import suggestion component

---
+++ src/app/components/suggestion/suggestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'suggestion',
+    templateUrl: 'suggestion.component.html'
+})
+export class SuggestionComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
