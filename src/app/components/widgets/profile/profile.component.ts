a4b03829fe932d0858f6781639ab66097cb02772
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-01 15:20:56 -0800

    widget profile component initial import

---
+++ src/app/components/widgets/profile/profile.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'widget-profile',
+    template: `
+    <pre>widget-profile template</pre>
+    `
+})
+export class WidgetProfileComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
