a2d9bb09db028b66e790c4da16c9ff3d5a377088
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-02 10:29:00 -0800

    basic widget

---
+++ src/app/components/widgets/live/widget.live.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'widget-live',
+    template: `
+        <pre>widget-live template</pre>
+    `
+})
+export class WidgetLiveComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
