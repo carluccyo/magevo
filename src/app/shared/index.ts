047ec12dcf47cf3aff7c60891019763c46f4d4b3
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-02 10:53:23 -0800

    mv tag in components

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,7 +1,6 @@
 export * from './api.service';
 export * from './calendar';
 export * from './item';
-export * from './tag';
 export * from './timeline';
 export * from './toast';
 export * from './window';
