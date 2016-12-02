990ae78ebdf458164cd752c0afe7c1fd702ae05f
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-02 10:26:47 -0800

    basic widget

---
+++ src/app/components/widgets/basic/widget.basic.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'widget-basic',
+    template: `
+    <pre>widget-basic template</pre>
+    `
+})
+export class WidgetBasicComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
