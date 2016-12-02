0f11c5faed621807cecd14152f6cf88b24e27bb1
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-02 10:56:29 -0800

    mv toast in components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,5 +2,6 @@ export * from './navbar';
 export * from './scroll-navbar';
 export * from './suggestion';
 export * from './tag';
+export * from './toast';
 export * from './widgets';
 export * from './components.module';
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,7 +2,6 @@ export * from './api.service';
 export * from './calendar';
 export * from './item';
 export * from './timeline';
-export * from './toast';
 export * from './window';
 
 
