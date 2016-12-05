e9123e8628020a35de64f56e2e63b37de254428a
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-05 11:55:24 -0800

    initial import timeline component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,6 +2,7 @@ export * from './navbar';
 export * from './scroll-navbar';
 export * from './suggestion';
 export * from './tag';
+export * from './timeline';
 export * from './toast';
 export * from './widgets';
 export * from './components.module';
---
+++ src/app/components/timeline/timeline.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'timeline',
+    template: ` <pre>timeline template here</pre>`
+})
+export class TimelineComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
