e629db3a665b79a15c10f6792f73fd73c63d3d70
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-06 10:49:01 -0800

    initial import progress bar component

---
+++ src/app/components/progressbar/progressbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'progress-bar',
+    template: ` <pre> progress bar template</pre>`
+})
+export class ProgressBarComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
