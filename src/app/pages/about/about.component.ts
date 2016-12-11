ef3fe0cfaa5cc28496037b83fbfb24177f2370f1
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-11 22:37:46 +0100

    about component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 
 export class AboutItem {
   id: String;
@@ -6,8 +6,18 @@ export class AboutItem {
 }
 
 export const ITEMS : [AboutItem] = [
-  {id: "1", title: 'title1'},
-  {id: "2", title: 'title2'}
+  {id: "1", title: 'title'},
+  {id: "2", title: 'title'},
+  // {id: "3", title: 'title'},
+  // {id: "4", title: 'title'},
+  // {id: "5", title: 'title'},
+  // {id: "6", title: 'title'},
+  // {id: "7", title: 'title'},
+  // {id: "8", title: 'title'},
+  // {id: "9", title: 'title'},
+  // {id: "10", title: 'title'},
+  // {id: "11", title: 'title'},
+  // {id: "12", title: 'title'}
 ]
 
 
@@ -16,14 +26,35 @@ export const ITEMS : [AboutItem] = [
   template:`
     <div class="container">
       <div class="row" >
-         <div *ngFor="let item of items; let i = index; trackBy: trackByFn">
+         <div [@move]='id' class="item col-md-2" *ngFor="let item of items; let i = index; trackBy: trackByFn">
+          <h3>{{item.title + item.id}}</h3>
+          <p>{{item.title}}</p>
+         </div>         
+      </div>
+      <div class="row" >
+         <div [@moveFast]='id' class="item col-md-2" *ngFor="let item of items; let i = index; trackBy: trackByFn">
           <h3>{{item.title + item.id}}</h3>
           <p>{{item.title}}</p>
          </div>         
       </div>
     </div>
   `,
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [
+    trigger('move', [
+      transition('void => *', [
+        style({transform: 'translateY(-100%)'}), 
+        animate(500)
+        ])
+    ]),
+    trigger('moveFast', [
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}), 
+        animate(100)
+        ])
+    ])
+    
+  ]
 })
 export class AboutComponent implements OnInit {
 
