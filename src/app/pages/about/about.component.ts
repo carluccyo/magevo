dacf7111773612dce17bf7c62bf2f68df6994dad
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-12 12:52:16 +0100

    about component animations

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
+import { Component, OnInit, trigger, state, style, group, transition, animate, keyframes } from '@angular/core';
 
 export class AboutItem {
   id: String;
   title: String;
 }
 
-export const ITEMS : [AboutItem] = [
-  {id: "1", title: 'title'},
-  {id: "2", title: 'title'},
-  // {id: "3", title: 'title'},
-  // {id: "4", title: 'title'},
-  // {id: "5", title: 'title'},
+export const ITEMS: [AboutItem] = [
+  { id: "1", title: 'title' },
+  { id: "2", title: 'title' },
+  { id: "3", title: 'title' },
+  { id: "4", title: 'title' },
+  { id: "5", title: 'title' },
   // {id: "6", title: 'title'},
   // {id: "7", title: 'title'},
   // {id: "8", title: 'title'},
@@ -23,16 +23,16 @@ export const ITEMS : [AboutItem] = [
 
 @Component({
   selector: 'my-about',
-  template:`
+  template: `
     <div class="container">
       <div class="row" >
-         <div [@move]='id' class="item col-md-2" *ngFor="let item of items; let i = index; trackBy: trackByFn">
+         <div [@flyInOut]='id' class="item col-md-2" *ngFor="let item of items; let i = index; trackBy: trackByFn">
           <h3>{{item.title + item.id}}</h3>
           <p>{{item.title}}</p>
          </div>         
       </div>
       <div class="row" >
-         <div [@moveFast]='id' class="item col-md-2" *ngFor="let item of items; let i = index; trackBy: trackByFn">
+         <div [@rotate]='id' [@moveFast]='id' class="item col-md-2" *ngFor="let item of items; let i = index; trackBy: trackByFn">
           <h3>{{item.title + item.id}}</h3>
           <p>{{item.title}}</p>
          </div>         
@@ -43,22 +43,53 @@ export const ITEMS : [AboutItem] = [
   animations: [
     trigger('move', [
       transition('void => *', [
-        style({transform: 'translateY(-100%)'}), 
-        animate(500)
-        ])
+        style({ transform: 'translateY(-100%)' }),
+        animate(1500)
+      ])
     ]),
     trigger('moveFast', [
       transition('void => *', [
-        style({transform: 'translateX(-100%)'}), 
-        animate(100)
+        style({ transform: 'translateX(-100%)' }),
+        animate(1100)
+      ])
+    ]),
+    trigger('rotate', [
+      transition('void => *', [
+        style({ transform: 'rotateZ(-180deg)' }),
+        animate(1100)
+      ])
+    ]),
+    trigger('flyInOut', [
+      state('in', style({ width: 120, transform: 'translateX(0)', opacity: 1 })),
+      transition('void => *', [
+        style({ width: 10, transform: 'translateX(50px)', opacity: 0 }),
+        group([
+          animate('0.7s 0.1s ease', style({
+            transform: 'translateX(0)',
+            width: 120
+          })),
+          animate('0.7s ease', style({
+            opacity: 1
+          }))
         ])
+      ]),
+      transition('* => void', [
+        group([
+          animate('0.9s ease', style({
+            transform: 'translateX(50px)',
+            width: 10
+          })),
+          animate('0.9s 0.2s ease', style({
+            opacity: 0
+          }))
+        ])
+      ])
     ])
-    
   ]
 })
 export class AboutComponent implements OnInit {
 
-  items : [AboutItem];  
+  items: [AboutItem];
 
   constructor() {
     // Do stuff
