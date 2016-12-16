3535e39358692b9abbbb002af5cde3715411c26b
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-12-16 13:42:20 +0100

    setup dashboard module

---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { routing } from './dashboard.routing';
+
+import { DashboardComponent } from './dashboard.component';
+
+import { Logger } from 'angular2-logger/core';
+
+import { LocationService } from 'angular2-geolocalitation/core';
+
+import { ItemListService } from '../../shared/item/item.list.service';
+import { ItemComponent } from '../../shared/item/item.component';
+import { ItemHorizontalComponent } from '../../shared/item/item.horizontal.component';
+import { ItemVerticalComponent } from '../../shared/item/item.vertical.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        routing
+    ],
+    declarations: [
+        DashboardComponent,
+        ItemComponent,
+        ItemHorizontalComponent,
+        ItemVerticalComponent
+    ],
+    providers: [
+        Logger,
+        LocationService,
+        ItemListService
+    ]
+})
+export class HomeModule { }
